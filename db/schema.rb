8b55dae7880900446aa3780e5c5413b84f3ff3d1
Adam Kelsey <adam.kelsey113@gmail.com>
2015-03-12 13:41:09 -0400

    created migration for the venues

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150312174033) do
+
+  create_table "venues", force: :cascade do |t|
+    t.integer "facebook_id"
+    t.string  "category"
+    t.string  "name"
+    t.string  "image"
+    t.string  "website_url"
+    t.string  "address_one"
+    t.string  "address_two"
+    t.string  "city"
+    t.string  "state"
+    t.integer "zip"
+    t.integer "phone_number"
+    t.text    "short_description"
+    t.text    "long_description"
+    t.text    "facebook"
+    t.text    "twitter"
+    t.boolean "birthday_party_venue"
+    t.text    "birthday_party_description"
+    t.string  "birthday_party_website_url"
+    t.integer "birthday_party_phone"
+  end
 
 end
